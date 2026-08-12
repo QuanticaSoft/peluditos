6c43874384f95d1eaa62d2223fba1c25945342c0
CRUXsur <marcojtorrico@hotmail.com>
2026-08-12 11:23:07 -0400

    refactor MainApp to use StatefulWidget and implement lifecycle management

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,44 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> with WidgetsBindingObserver {
+
+  @override
+  void initState() {
+    super.initState();
+    
+    WidgetsBinding.instance.addObserver(this);
+
+  }
+
+  @override
+  void dispose() {
+    
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    //print('AppLifecycleState: $state');
+    /*
+      resumed: la app paso de estar del background al foreground
+      inactive: la app esta en foreground pero no responde a eventos del usuario, no esta en focus
+      paused: la app paso de estar del foreground al background
+      detached: la app esta terminando y no se puede acceder a ella, app fue destruiuda, tenemos muy poco teimpo de reaccion para guardar informacion, se recomienda usarlo para guardar informacion en local storage
+      hidden: la app esta en background y no es visible para el usuario, pero sigue corriendo, se recomienda usarlo para guardar informacion en local storage
+    */
+    super.didChangeAppLifecycleState(state);
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -16,4 +52,6 @@ class MainApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
     );
   }
+
+
 }
